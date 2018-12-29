c2b71600bb7b6d1615d9ae5434d5c6403417f6b5
ioprojecton <46145992+ioprojecton@users.noreply.github.com>
2018-12-28 20:55:33 -0500

    Update gameoflife.ino

--- gameoflife.ino
+++ gameoflife.ino
@@ -1,4 +1,4 @@
-// Written by Caleb LaFeve 
+// Written by Caleb LaFeve
 //Conway's game of life
 
 
@@ -32,20 +32,24 @@
 #define WIDTH 32
 #define HEIGHT 32
 
+int checkSum();
+void reset();
+void writeNextGeneration();
+void update();
 
 RGBmatrixPanel matrix(A, B, C, D, CLK, LAT, OE, false);
-//May need to change the lines above if you're using a different board or LED matrix than the 32x32 
+//May need to change the lines above if you're using a different board or LED matrix than the 32x32
 
-int r, g, b;
-int counter = 0;
-int cells[HEIGHT][WIDTH];
-int newCells[HEIGHT][WIDTH];
+byte r, g, b;
+unsigned int counter = 0;
+byte cells[HEIGHT][WIDTH];
+byte newCells[HEIGHT][WIDTH];
 int sum1 = 0;
 
 
 
 void setup() {
-  //Color 
+  //Color
   r = 1;
   g = 0;
   b = 0;
@@ -54,81 +58,74 @@ void setup() {
   //0 == Dead
   //1 == Alive
 
-  for(unsigned int row = 0; row < WIDTH; row++) for(unsigned int col = 0; col < HEIGHT; cells[row][col++] = random(0,2));
-  
+  for (byte row = 0; row < WIDTH; row++) for (byte col = 0; col < HEIGHT; cells[row][col++] = random(0, 2));
+
   matrix.begin();
 
 }
 
 void loop() {
-  
+
   counter++;
-  
-  if(counter == resetTime ){
-    sum1 = checkSum();
-    
-  }
-  if(counter == resetTime + 1){
-    if(checkSum() == sum1)
-      reset();
+
+  if (counter == resetTime ) sum1 = checkSum();
+
+  if (counter == resetTime + 1) {
+    if (checkSum() == sum1) reset();
     counter = 0;
     sum1 = 0;
-}
-  
+  }
+
   update();
   delay(animationSpeed);
   writeNextGeneration();
-  
+
 
 }
-int checkSum(){
+int checkSum() {
   int sum = 0;
-  for(unsigned int row = 0; row < WIDTH; row++) for(unsigned int col = 0; col < HEIGHT; cells[row][col++]) sum += cells[row][col];
+  for (byte row = 0; row < WIDTH; row++) for (byte col = 0; col < HEIGHT; cells[row][col++]) sum += cells[row][col];
   return sum;
 }
 //Resets 2D araay back to randomness
-void reset(){
-  for(unsigned int row = 0; row < WIDTH; row++) for(unsigned int col = 0; col < HEIGHT; cells[row][col++] = random(0,2));
-  counter=0;
+void reset() {
+  for (unsigned int row = 0; row < WIDTH; row++) for (unsigned int col = 0; col < HEIGHT; cells[row][col++] = random(0, 2));
+  counter = 0;
 }
 //This method checks every cell(pixel) and check to see how many neighbors it has
 //The amount of neighbors determines its future state
-void writeNextGeneration(){
-  for(int row = 0; row < WIDTH; row++){
-    for(int col = 0; col < HEIGHT; col++){
-      newCells[row][col] = cells[row][col];
-    }
-  }
-  
-  for(unsigned int row = 0; row < WIDTH; row++){
-    for(unsigned int col = 0; col < HEIGHT; col++){
-      int surroundingCells = 0;
-     
-      boolean isAlive=false;
+void writeNextGeneration() {
+  for (byte row = 0; row < WIDTH; row++) for (byte col = 0; col < HEIGHT; col++) newCells[row][col] = cells[row][col];
 
-      cells[row][col]?isAlive = true:isAlive = false;
-      
-      for(int i = -1; i < 2; i++) for(int j = -1; j < 2; j++) surroundingCells += cells[crNum(row,i,WIDTH)][crNum(col,j,HEIGHT)];
-      
+  for (byte row = 0; row < WIDTH; row++) {
+    for (byte col = 0; col < HEIGHT; col++) {
+      int surroundingCells = 0;
+
+      boolean isAlive = false;
+
+      cells[row][col] ? (isAlive = true) : (isAlive = false);
+
+      for (int i = -1; i < 2; i++) for (int j = -1; j < 2; j++) surroundingCells += cells[crNum(row, i, WIDTH)][crNum(col, j, HEIGHT)];
+
       surroundingCells -= cells[row][col];
       //Check neighboring cells and store its future state in a new 2D array
-      if((surroundingCells < 2 || surroundingCells > 3) && isAlive) newCells[row][col] = 0;
- 
-      if(surroundingCells == 3 && !isAlive) newCells[row][col] = 1;
-      
+      if ((surroundingCells < 2 || surroundingCells > 3) && isAlive) newCells[row][col] = 0;
+
+      if (surroundingCells == 3 && !isAlive) newCells[row][col] = 1;
+
     }
   }
   //copy new 2d Array to old
-  for(int row = 0; row < WIDTH; row++) for(int col = 0; col < HEIGHT; col++) cells[row][col] = newCells[row][col];
-  
+  for (byte row = 0; row < WIDTH; row++) for (byte col = 0; col < HEIGHT; col++) cells[row][col] = newCells[row][col];
+
 }
 //Update pixels
-void update(){
-  for(unsigned int row = 0; row < WIDTH; row++)
-    for(unsigned int col = 0; col < HEIGHT; col++) 
-      cells[row][col]?matrix.drawPixel(row, col, matrix.Color333(r, g,b)): matrix.drawPixel(row, col, matrix.Color333(0,0,0));
+void update() {
+  for (byte row = 0; row < WIDTH; row++)
+    for (byte col = 0; col < HEIGHT; col++)
+      cells[row][col] ? matrix.drawPixel(row, col, matrix.Color333(r, g, b)) : matrix.drawPixel(row, col, matrix.Color333(0, 0, 0));
 
-        //Random colors/party mode ;)
-        //matrix.drawPixel(row, col, matrix.Color333(random(7), random(7),random(7)));
-        //If you uncomment the above line, make sure to comment the line below
+  //Random colors/party mode ;)
+  //matrix.drawPixel(row, col, matrix.Color333(random(7), random(7),random(7)));
+  //If you uncomment the above line, make sure to comment the line below
 }
